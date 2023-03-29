15c334fbe4a0a666ec40ca92f417ce620e75ef7d
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-03-29 09:23:31 -0300

    feat: disable subscription button

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -1,25 +1,22 @@
 import { FaCalendarAlt, FaExternalLinkAlt } from "react-icons/fa";
+import { Button } from "react-scroll";
 import DefaultAnimationContainer from "./common/DefaultAnimation";
 
 function Subscription() {
   return (
     <DefaultAnimationContainer>
       <div className="flex flex-col items-center gap-3">
-        <div className="flex flex-col items-center gap-2 text-center text-2xl font-bold text-white md:flex-row lg:text-6xl">
-          <FaCalendarAlt size={32} />
-          Inscrições abertas até o dia 10/04/2023
-        </div>
-        <a
-          className="bg-primary w-fit rounded-lg p-5 transition-all hover:scale-105"
-          href="https://forms.gle/8y2yAVo61fzWQHLt9"
-          target="_blank"
-          rel="noreferrer noopener"
+        <button
+          className="
+          bg-primary
+          w-fit rounded-lg
+          p-5 transition-all  hover:scale-105 disabled:opacity-30"
+          disabled
         >
           <div className="flex items-center gap-2 text-3xl font-semibold text-white">
-            <h1>Quero me inscrever!</h1>
-            <FaExternalLinkAlt size={24} />
+            <h1>Inscrições finalizadas!</h1>
           </div>
-        </a>
+        </button>
       </div>
     </DefaultAnimationContainer>
   );
